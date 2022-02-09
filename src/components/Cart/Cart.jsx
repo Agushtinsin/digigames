c233db09cdec3165e14c52de5ceba3757a898d92
Agustín Lezcano <aguslez05@gmail.com>
2022-02-09 11:10:37 -0300

    Clase 11

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,12 +2,31 @@ import React from "react";
 import ItemDetail from "../ItemDetail/ItemDetail";
 
 const Cart = ({ items }) => {
+    const total = items.map((currentItem) => {
+        return currentItem.item.price * currentItem.cantidad;
+    });
+
+    function sumar() {
+        let count = 0;
+        total.forEach((element) => {
+            count += element;
+        });
+        return count;
+    }
+
     return (
         <div>
-            {items.map((currentItem) => {
-                const { item } = currentItem;
-                return <ItemDetail {...item} cantidad={currentItem.cantidad} />;
-            })}
+            <div className="cards">
+                {items.map((currentItem) => {
+                    const { item } = currentItem;
+                    return (
+                        <ItemDetail {...item} cantidad={currentItem.cantidad} />
+                    );
+                })}
+            </div>
+            <div style={{ textAlign: "center" }}>
+                <h2>Su total es de: {sumar()}</h2>
+            </div>
         </div>
     );
 };
--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../CartContext/CartContext";
 
 export default function CartWidget() {
+    const { itemsCart } = useContext(CartContext);
+
+    function totalCart() {
+        let count = 0;
+        itemsCart.forEach((element) => {
+            count += element.cantidad;
+        });
+        return count;
+    }
+
     return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-        >
-            <path
-                className="Carrito"
-                d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z"
-            />
-        </svg>
+        <li style={{ visibility: totalCart() > 0 ? "visible" : "hidden" }}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+            >
+                <path
+                    className="Carrito"
+                    d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z"
+                />
+            </svg>
+            <p>{totalCart()}</p>
+        </li>
     );
 }
